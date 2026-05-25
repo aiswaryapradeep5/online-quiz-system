1555fcc86fd424f68604b130b66cee349c0369cb
aiswaryapradeep5 <aiswaryapradeep.546@gmail.com>
2026-05-25 08:46:37 +0530

    fix quiz api routes

--- server/src/routes/attempt.routes.js
+++ server/src/routes/attempt.routes.js
@@ -1,5 +1,5 @@
 const r = require('express').Router();
-const c = require('../controllers/attempt.controller');
+const c = require('../controllers/api/attempt.controller');
 const { requireAuth } = require('../middleware/auth');
 const validate = require('../middleware/validate');
 const s = require('../validators/schemas');
@@ -8,7 +8,7 @@ r.get('/leaderboard/:quizId', c.leaderboard);
 
 r.use(requireAuth);
 
-r.post('/start', validate(s.startAttempt), c.start);
+r.post('/api/start', validate(s.startAttempt), c.start);
 r.post('/:id/submit', validate(s.submitAttempt), c.submit);
 r.get('/me', c.myAttempts);
 module.exports = r;
